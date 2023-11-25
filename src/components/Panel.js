ce804f5c58b78f915196a5265ac851246e87f4b2
Cyrus Baybay <41849568+TRA3H@users.noreply.github.com>
2023-11-24 20:59:32 -0800

    fix
    fix

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,43 +1,43 @@
-
 import React, { useState } from 'react';
 import Display from './Display';
 import CalculatorButton from './CalculatorButton';
+import { evaluate } from 'mathjs';
 
 const Panel = () => {
     const [input, setInput] = useState('');
     const [result, setResult] = useState('');
-  
+
     const handleInput = (e) => {
-      setInput(input + e.target.name);
+        setInput(input + e.target.name);
     };
-  
+
     const calculate = () => {
-      try {
-        setResult(eval(input));
-      } catch (error) {
-        setResult('Error');
-      }
+        try {
+            setResult(evaluate(input));
+        } catch (error) {
+            setResult('Error');
+        }
     };
-  
+
     const clear = () => {
-      setInput('');
-      setResult('');
+        setInput('');
+        setResult('');
     };
-  
+
     return (
-      <div className='calculator'>
-        <Display input={input} result={result} />
-        <div className='calculator-buttons'>
-          {['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', 'C', '=', '/'].map((button) => (
-            button === 'C' ? 
-              <CalculatorButton key={button} name={button} onClick={clear} /> :
-              button === '=' ? 
-                <CalculatorButton key={button} name={button} onClick={calculate} /> :
-                <CalculatorButton key={button} name={button} onClick={handleInput} />
-          ))}
+        <div className='calculator'>
+            <Display input={input} result={result} />
+            <div className='calculator-buttons'>
+                {['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', 'C', '=', '/'].map((button) => (
+                    button === 'C' ? 
+                        <CalculatorButton key={button} name={button} onClick={clear} /> :
+                        button === '=' ? 
+                            <CalculatorButton key={button} name={button} onClick={calculate} /> :
+                            <CalculatorButton key={button} name={button} onClick={handleInput} />
+                ))}
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default Panel;
+};
+
+export default Panel;
